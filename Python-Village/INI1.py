8a3f69e5b49daf37928116b4137565734bc59523
Pramod-Shrinivas <pramod.shrinivas@hotmail.com>
2015-01-22 19:57:01 +0530

    Create INI1.py

---
+++ Python-Village/INI1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+'''
+ROSALIND problems from the Python Village Section
+Problem Title: Installing Python
+Problem ID: INI1
+URL: http://rosalind.info/problems/ini1/
+'''
+import this
